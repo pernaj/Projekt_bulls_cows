5a0d7237f11bb99340526a46bc81e1a094f88481
pernaj <jana.perna@seznam.cz>
2026-01-04 12:06:40 +0100

    created functions to validate guessed number for correct length, only digits, no zero in the first position, no duplicity.

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ print("Hi there!","-" * 50,
       "Let's play a bulls and cows game.", "-" * 50, "Enter a number:", 
       "-" * 50, sep="\n")
 
-# Programme randomly generates secret 4-digit number; 
-# no zero in the first position; no duplicity.
+# Programme randomly generates secret number; 
+# 4 digits, no zero in the first position; no duplicity.
 
 code = []
 while len(code) < CODE_LENGTH:
@@ -25,16 +25,42 @@ while len(code) < CODE_LENGTH:
     else:
         code.append(number)
 
-# Guess number verification: correct length, only numbers, no zero at first position, no duplicity.
-if len(guess) != 4:
-    print("Incorrect length. Enter exactly 4 digit number!")
-elif not guess.isdigit():
-    print("Incorrect input. Enter only numbers!")
-elif guess[0] == '0':
-    print("Zero cannot be entered in the first position!")
-elif len(guess) != len(set(guess)):
-    print("Duplicity. The number cannot contain repeated digits!")
-else:
-    print("bulls, cows...")
+# Functions to validate if initial conditions for guessed number are fulfilled:
+# correct length, only numbers, no zero at first position, no duplicity.
+
+def check_length(x: str) -> str | None:
+    """ Validates if the entered number has correct length.
+    Returns error message or None.
+    """
+    if len(x) != CODE_LENGTH:
+        return f"Error: invalid length. Enter exactly {CODE_LENGTH} digit number!"
+    return None
+
+def check_digits(x: str) -> str | None:
+    """Validates if the entered number contains only digits.
+    Returns error message or None.
+    """
+    if not x.isdigit():
+        return f"Error: entered no digits. Only digits are allowed!"
+    return None
+
+def check_zero(x: str) -> str | None:
+    """Validates if there is no zero in the first position of the entered number.
+    Returns error message or None.
+    """
+    if not x:
+        return f"Error: empty input. Enter exactly {CODE_LENGTH} digit number!"
+    if x[0] == '0':
+        return f"Error: leading zero. Zero cannot be entered in the first position!"
+    return None
+
+def check_duplicity(x: str) -> str | None:
+    """Validates if there are only unique numbers in the entered number.
+    Returns error message or None.
+    """
+    if len(x) != len(set(x)):
+        return f"Error: duplicity. The number cannot contain repeated digits!"
+    return None 
+
 
 guess = input(">>> ")
